d902305c54a91c4d655fd88384e06d43c585300a
Alperen Diler <alperendiler1@gmail.com>
2024-01-03 07:06:43 +0300

    latest

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,60 +1,63 @@
 import React  , {useEffect, useState} from 'react'
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import { Icon,  Menu, Table } from 'semantic-ui-react'
 import ProductServices from '../Services/productService'
+import { Link } from 'react-router-dom'
 
 export default function ProductList() {
-const [products, setproducts] = useState([])
+const [products, setProducts] = useState([])
 
 useEffect(()=>{
-  let productService = new ProductServices();
-  productService.getProducts().then(result=>setproducts(result.data));   
-})
+  fetchProducts();
+},[])
+
+const fetchProducts = () => {
+  ProductServices.getProducts().then(response=> {
+    setProducts(response.data.products);
+  });
+};
 
   return (
     <div> <Table celled>
     <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Ürün Adı</Table.HeaderCell>
-            <Table.HeaderCell>Birim Fiyatı</Table.HeaderCell>
-            <Table.HeaderCell>Stok Adeti</Table.HeaderCell>
-            <Table.HeaderCell>Açıklama</Table.HeaderCell>
-            <Table.HeaderCell>Kategori</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Ürün Adı</Table.HeaderCell>
+        <Table.HeaderCell>Stok adedi</Table.HeaderCell>
+        <Table.HeaderCell>Fiyat</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
 
     <Table.Body>
    
      {
         products.map(product=>(
-            <Table.Row >
-            <Table.Cell>{product.productName}</Table.Cell>
-            <Table.Cell>{product.unitsInStock}</Table.Cell>
-            <Table.Cell>{product.unitPrice}</Table.Cell>
+            <Table.Row key={product.id} >
+            <Table.Cell  ><Link to={"/products/" + product.id}>{product.title}</Link></Table.Cell>
+            <Table.Cell >{product.stock}</Table.Cell>
+            <Table.Cell >{product.price}</Table.Cell>
           </Table.Row>
         ))
      }
      
     </Table.Body>
 
-        <Table.Footer>
-          <Table.Row>
-            <Table.HeaderCell colSpan="5">
-              <Menu floated="right" pagination>
-                <Menu.Item as="a" icon>
-                  <Icon name="chevron left" />
-                </Menu.Item>
-                <Menu.Item as="a">1</Menu.Item>
-                <Menu.Item as="a">2</Menu.Item>
-                <Menu.Item as="a">3</Menu.Item>
-                <Menu.Item as="a">4</Menu.Item>
-                <Menu.Item as="a" icon>
-                  <Icon name="chevron right" />
-                </Menu.Item>
-              </Menu>
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Footer>
-      </Table>
-      </div>
+    <Table.Footer>
+      <Table.Row>
+        <Table.HeaderCell colSpan='3'>
+          <Menu floated='right' pagination>
+            <Menu.Item as='a' icon>
+              <Icon name='chevron left' />
+            </Menu.Item>
+            <Menu.Item as='a'>1</Menu.Item>
+            <Menu.Item as='a'>2</Menu.Item>
+            <Menu.Item as='a'>3</Menu.Item>
+            <Menu.Item as='a'>4</Menu.Item>
+            <Menu.Item as='a' icon>
+              <Icon name='chevron right' />
+            </Menu.Item>
+          </Menu>
+        </Table.HeaderCell>
+      </Table.Row>
+    </Table.Footer>
+  </Table></div>
   )
 }
